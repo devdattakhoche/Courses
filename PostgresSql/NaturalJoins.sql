1b13946da2ef677223a7b67230388d0fa1011290
devdattakhoche <dkhoche70@gmail.com>
2021-04-05 17:15:20 +0530

    Joins completed

--- PostgresSql/NaturalJoins.sql
+++ PostgresSql/NaturalJoins.sql
@@ -1,3 +1,5 @@
+/* Natural Joins*/ 
+ 
 select  companyname,orderdate,shipcountry from orders join customers on customers.customerid = orders.customerid;
 select firstname ,lastname,orderdate from employees join orders on employees.employeeid = orders.employeeid;
 select companyname,unitprice,unitsinstock from products join suppliers on products.supplierid = suppliers.supplierid; 
@@ -27,3 +29,47 @@ join customers on customers.customerid=orders.customerid
 join products on products.productid=order_details.productid
 join categories on categories.categoryid=products.categoryid
 where categoryname='Seafood' and order_details.unitprice*quantity >= 500;;
+
+
+/* Left Joins  */
+select productname , orderid from products 
+left join order_details 
+on order_details.productid = products.productid;
+
+select productname,orderid from products 
+left join order_details 
+on order_details.productid = products.productid where orderid=NULL;
+
+
+/* Right Joins  */
+select customercustomerdemo.customerid,companyname 
+from customercustomerdemo 
+right join customers 
+on customers.customerid = customercustomerdemo.customerid;
+
+
+/* Full Joins  */
+
+select categoryname , productname 
+from categories full join products 
+on categories.categoryid = products.categoryid;
+
+/* Self Joins */
+select e.companyname as companyname1 ,
+q.companyname as companyname2, q.country
+from suppliers as e join suppliers as q
+on e.country = q.country 
+where e.companyname <> q.companyname
+order by e.country ;
+
+/* Using "Using" */
+select * from orders 
+join order_details using(orderid)
+join products using(productid);
+
+/* Natural Joins */
+select * from customers
+natural join orders
+natural join order_details;
+
+
