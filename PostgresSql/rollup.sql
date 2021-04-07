cb3e4dc3822b6e7a4222ce134b63dc3b88bbc7b9
devdattakhoche <dkhoche70@gmail.com>
2021-04-07 14:18:40 +0530

    🐛 roll up in group by

---
+++ PostgresSql/rollup.sql
@@ -0,0 +1,17 @@
+select c.companyname,categoryname,productname,sum(od.unitprice*quantity)
+from customers as c
+natural join orders
+natural join order_details as od
+join products using (productid)
+join categories  using (categoryid)
+group by rollup(companyname, categoryname, productname);
+order by companyname,categoryname,productname
+
+select s.companyname as supplier, c.companyname as buyer,productname, sum(od.unitprice*quantity)
+from suppliers as s
+join products using (supplierid)
+join order_details as od using (productid)
+join orders using (orderid)
+join customers as c using (customerid)
+group by rollup(supplier, buyer, productname)
+order by supplier,buyer,productname;
